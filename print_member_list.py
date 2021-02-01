87e93c5afbb343b1f290ab463ce069a6826535f6
wndyb <77156313+wndyb@users.noreply.github.com>
2021-02-01 09:54:34 -0500

    Update print_member_list.py

--- print_member_list.py
+++ print_member_list.py
@@ -1,2 +1,2 @@
 # This is a file experimenting with collaborating on GitHub. The goal is to work with a team to print a member_list
-print("The following people are participating in the GitHub mini-workshop")
+print("The following people are participating in the GitHub mini-workshop:")
